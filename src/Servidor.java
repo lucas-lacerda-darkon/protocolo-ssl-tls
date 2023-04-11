f445e5377d454d1fca1d0c9fe390c11951e1449c
cancio <filipe.cancio@outlook.com>
2023-04-10 21:10:29 -0300

    criando o server

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,20 +1,76 @@
+import com.sun.net.httpserver.*;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyStore;
 import java.util.Scanner;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.TrustManagerFactory;
+
 public class Servidor {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5000);
-        System.out.println("Servidor iniciado.");
+    public static void main(String[] args) throws IOException, Exception {
 
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Cliente conectado.");
+        char[] passphrase = "senha senha".toCharArray();
+    
+        // carrega o arquivo de keystore
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream("keystore.jks"), passphrase);
 
-        Scanner scanner = new Scanner(clientSocket.getInputStream());
-        while (true) {
-            String message = scanner.nextLine();
-            System.out.println("Mensagem recebida: " + message);
-        }
+        // inicializa o gerenciador de chaves
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, passphrase);
+
+        // inicializa o gerenciador de confiança
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(ks);
+
+        // cria um contexto SSL
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+        
+        // cria o servidor HTTPS
+        HttpsServer httpsServer = HttpsServer.create(new InetSocketAddress(5000), 0);
+        httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+            public void configure(HttpsParameters params) {
+                try {
+                    // inicializa o SSL engine
+                    SSLContext context = SSLContext.getDefault();
+                    SSLEngine engine = context.createSSLEngine();
+                    engine.setNeedClientAuth(false);
+                    params.setNeedClientAuth(false);
+                    params.setCipherSuites(engine.getEnabledCipherSuites());
+                    params.setProtocols(engine.getEnabledProtocols());
+                    SSLParameters defaultSSLParameters = context.getDefaultSSLParameters();
+                    params.setSSLParameters(defaultSSLParameters);
+                } catch (Exception e) {
+                    System.err.println("Falha na inicialização do SSL engine: " + e.getMessage());
+                }
+            }
+        });
+
+        // cria o manipulador HTTP
+        httpsServer.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Olá, mundo!";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+
+        // inicia o servidor HTTPS
+        httpsServer.start();
+        System.out.println("Servidor HTTPS iniciado.");
     }
 }
